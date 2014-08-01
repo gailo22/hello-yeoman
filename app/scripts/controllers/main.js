6e537dfdaff9b70bf3f0907e5ccffc83b14e4bd3
gailo22 <gailo22@gmail.com>
2014-08-01 14:58:31 +0700

    add todos list

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,15 @@
  * Controller of the helloYeomanApp
  */
 angular.module('helloYeomanApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+    .controller('MainCtrl', function($scope) {
+        $scope.todos = ['Item 1', 'Item 2', 'Item 3'];
+
+        $scope.addTodo = function() {
+            $scope.todos.push($scope.todo);
+            $scope.todo = '';
+        };
+
+        $scope.removeTodo = function(index) {
+            $scope.todos.splice(index, 1);
+        };
+    });
--- test/spec/controllers/main.js
+++ test/spec/controllers/main.js
@@ -1,22 +1,23 @@
 'use strict';
 
-describe('Controller: MainCtrl', function () {
+describe('Controller: MainCtrl', function() {
 
-  // load the controller's module
-  beforeEach(module('helloYeomanApp'));
+    // load the controller's module
+    beforeEach(module('helloYeomanApp'));
 
-  var MainCtrl,
-    scope;
+    var MainCtrl,
+        scope;
 
-  // Initialize the controller and a mock scope
-  beforeEach(inject(function ($controller, $rootScope) {
-    scope = $rootScope.$new();
-    MainCtrl = $controller('MainCtrl', {
-      $scope: scope
+    // Initialize the controller and a mock scope
+    beforeEach(inject(function($controller, $rootScope) {
+        scope = $rootScope.$new();
+        MainCtrl = $controller('MainCtrl', {
+            $scope: scope
+        });
+    }));
+
+    it('should have no items to start', function() {
+        expect(scope.todos.length).toBe(0);
     });
-  }));
 
-  it('should attach a list of awesomeThings to the scope', function () {
-    expect(scope.awesomeThings.length).toBe(3);
-  });
 });
